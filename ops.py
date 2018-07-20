01fba5a3663e2cc51cbdc52cd050f577920bf40e
mx54039q <414179190@qq.com>
2018-07-20 10:37:55 +0800

    modify 'instance normalization'. 2018/7/20

--- ops.py
+++ ops.py
@@ -5,31 +5,16 @@ import tensorflow as tf
 from config import cfg
 import tensorflow.contrib.slim as slim
 
-def ins_norm(input, reuse=True, name="in"):
-    """Instance Normalization
-    
-    args:
-        input: input tensor;
-        reuse (tf.bool): whether to reuse parameter
-        name (string): name of this op
-    return:
-        instance normalized result 
-    """
-    with tf.variable_scope(name, reuse=reuse):
-        eps = 1e-5
-        mean, sigma = tf.nn.moments(input, [1, 2], keep_dims=True)
-        normalized = (input - mean) / (tf.sqrt(sigma) + eps)
-    return normalized
-    
-def pixel_norm(x, train=True, name="pn"):
-    '''Pixel channel-wise normalization
-    
-    Pixe channel-wise normalization from "PROGRESSIVE GROWING OF GANS FOR
-    IMPROVED QUALITY, STABILITY, AND VARIATION"
-    '''
+def instance_norm(input, name="instance_norm"):
     with tf.variable_scope(name):
-        norm_axis = len(x.get_shape()) - 1
-        return x * tf.rsqrt(tf.reduce_mean(tf.square(x), axis=norm_axis, keep_dims=True) + 1e-8)
+        depth = input.get_shape()[3]
+        scale = tf.get_variable("scale", [depth], initializer=tf.random_normal_initializer(1.0, 0.02, dtype=tf.float32))
+        offset = tf.get_variable("offset", [depth], initializer=tf.constant_initializer(0.0))
+        mean, variance = tf.nn.moments(input, axes=[1,2], keep_dims=True)
+        epsilon = 1e-5
+        inv = tf.rsqrt(variance + epsilon)
+        normalized = (input-mean)*inv
+        return scale*normalized + offset
 
 def bn(x, train=True, name="bn", epsilon=1e-5, momentum = 0.9):
     """Batch Normalization implemented by tensorflow
@@ -101,31 +86,31 @@ def local(x,filters,name,kernel_size=3,strides=[1,1],padding='valid'):
 def conv2d(inputs, filters, name, kernel_size = 3, strides = 1, padding='same', bias=cfg.use_bias,
            dilation_rate = 1, trainable = True, activation = None, reuse = False):
     return tf.layers.conv2d(inputs, filters = filters,
-             kernel_size = kernel_size,
-             padding = padding,
-             strides = strides,
-             dilation_rate = dilation_rate,
-             activation = activation,
-             trainable = trainable,
-             reuse = reuse,
-             use_bias = bias,
-             kernel_initializer = tf.truncated_normal_initializer(stddev=cfg.stddev),
-             kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
-             name = name)
+                            kernel_size = kernel_size,
+                            padding = padding,
+                            strides = strides,
+                            dilation_rate = dilation_rate,
+                            activation = activation,
+                            trainable = trainable,
+                            reuse = reuse,
+                            use_bias = bias,
+                            kernel_initializer = tf.truncated_normal_initializer(stddev=cfg.stddev),
+                            kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
+                            name = name)
 
 def deconv2d(inputs, filters, name, kernel_size = 3, strides = 1, padding='same',
-           trainable = True, activation = None, reuse = False, bias=cfg.use_bias):
+             trainable = True, activation = None, reuse = False, bias=cfg.use_bias):
     return tf.layers.conv2d_transpose(inputs, filters = filters,
-             kernel_size = kernel_size,
-             padding = padding,
-             strides = strides,
-             activation = activation,
-             trainable = trainable,
-             reuse = reuse,
-             use_bias = bias,
-             kernel_initializer = tf.truncated_normal_initializer(stddev=cfg.stddev),
-             kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
-             name = name)
+                                      kernel_size = kernel_size,
+                                      padding = padding,
+                                      strides = strides,
+                                      activation = activation,
+                                      trainable = trainable,
+                                      reuse = reuse,
+                                      use_bias = bias,
+                                      kernel_initializer = tf.truncated_normal_initializer(stddev=cfg.stddev),
+                                      kernel_regularizer = tf.contrib.layers.l2_regularizer(0.0001),
+                                      name = name)
 
 def deconv2d_w(input_, output_shape,
              k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
@@ -165,11 +150,11 @@ def fullyConnect(inputs, units, name, bias=cfg.use_bias, trainable = True, activ
 def lrelu(x, leak=0.2, name="lrelu"):
     return tf.maximum(x, leak*x)
 
-def res_block(inputs, name, is_train, normal='bn',kernel_size = 3, strides = 1, padding='same', bias=cfg.use_bias):
-    """Residual block with batch normalization
-    """
+def res_block(inputs, name, is_train, normal='bn',kernel_size = 3,
+              strides = 1, padding='same', bias=cfg.use_bias):
+    """Residual block with batch normalization or instance norm"""
     with tf.variable_scope(name):
-        norm = bn if(normal=='bn') else pixel_norm 
+        norm = bn if(normal=='bn') else instance_norm
         filters = inputs.get_shape().as_list()[-1]
         conv1 = tf.nn.relu(norm(conv2d(inputs, filters, 'conv1', 
                                 kernel_size=kernel_size, strides = strides),is_train,'norm1'))
@@ -178,8 +163,7 @@ def res_block(inputs, name, is_train, normal='bn',kernel_size = 3, strides = 1, 
         return tf.nn.relu(tf.add(inputs, conv2))
 
 def res_block_ln(inputs, name, kernel_size = 3, strides = 1, padding='same', bias=cfg.use_bias):
-    """Residual block with layer normalization
-    """
+    """Residual block with layer normalization"""
     with tf.variable_scope(name):
         filters = inputs.get_shape().as_list()[-1]
         conv1 = tf.nn.relu(slim.layer_norm(conv2d(inputs, filters, 'conv1', 
